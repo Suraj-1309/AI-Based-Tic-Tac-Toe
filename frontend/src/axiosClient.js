7b305e1350b5d55ec7153bdf1e448b0a449f731c
Suraj-1309 <surajsinghch2055@gmail.com>
2025-12-17 12:30:44 +0530

    add logo on the top

--- frontend/src/axiosClient.js
+++ frontend/src/axiosClient.js
@@ -1,18 +1,18 @@
 import axios from "axios";
-import { API_URL } from "./config";
+import { API_BASE_URL } from "./config";
 
 const axiosClient = axios.create({
-    baseURL : API_URL,
-    headers : {
-        "Content-Type" : "application/json",
-    },
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 axios.interceptors.request.use(
-    (config) => {
-        return config;
-    },
-    (error) => Promise.reject(error)
+  (config) => {
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 export default axiosClient;
